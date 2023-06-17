b7a109fdcf6e7b2fe9c59a105a75f55febe29e9e
Wdson Danrlei <wdson91@gmail.com>
2023-06-17 12:32:05 -0300

    angular build

--- backend/tour-of-heroes/src/app.module.ts
+++ backend/tour-of-heroes/src/app.module.ts
@@ -2,16 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeroesModule } from './heroes/heroes.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 /* eslint-disable */
 @Module({
 
   imports: [
     HeroesModule,
-    // ServeStaticModule.forRoot({
-    //   rootPath: join(__dirname, '../angular-build '),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../angular-build '),
 
-    // }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
